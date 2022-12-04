c4cb2d7002b3e8ac5c1521bcff2e07417641ea45
arr09 <38495998+arr09@users.noreply.github.com>
2022-12-04 20:23:47 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 05, 2022',
+  birthdate: 'Dec 05, 2020',
   name: 'Ciaa'
 };
 
